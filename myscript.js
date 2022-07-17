a2da5fc6b3f5fbe48ca674124ba36964b8efeca0
pedro <paarruvito@hotmail.com>
2022-07-17 00:36:47 -0300

    subo proyecto

---
+++ myscript.js
@@ -0,0 +1,50 @@
+function mostrar(){ 
+
+  document.getElementById('playlist').style.display  = 'block';
+  document.getElementById('preguntas').style.display = 'none' ;
+  document.getElementById('back').style.display = 'block' ;
+  document.getElementById('mostrar').style.display = 'none' ;
+  
+  }
+
+/*
+function calcular(){
+
+    var monto = document.getElementById("dolares").value;
+    var altura = document.getElementById("altura").value;
+    var peso = document.getElementById("peso").value;
+    var edad = document.getElementById("edad").value;
+    
+    if ((monto > 9999 && monto < 99999) && altura > 0 && peso > 1 && edad > 16){
+      swal({
+        title: "FELICITACIONES!!",
+        text: "Hemos chequeado tu saldo bancario y eres Lindo!",
+        icon: "success",
+        button: "click aqui",
+        });
+      }
+      else if (monto > 100000 && altura > 0 && peso > 1 && edad > 16){
+        swal({
+          title: "WOW, ERES DESLUMBRANTE!!",
+          text: "Hemos chequeado tu saldo bancario y eres tan bello que deberiamos casarnos!",
+          icon: "success",
+          button: "click aqui",
+        });
+      }
+        else{
+          swal({
+            title: "OH NO!!",
+            text: "NO ERES LINDO!",
+            icon: "error",
+            button: "click aqui",
+          });
+        }
+    }
+    
+    function limpiar() {
+        document.getElementById("botones").reset();
+    }
+    
+    */ 
+    
+    
